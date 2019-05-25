7cd49fb8b434caeb809a72b6859e31dbc58d0249
Britta <polished.athena@gmail.com>
2019-05-24 21:12:48 -0500

    updated seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,9 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 CREATE TABLE burgers (
-  id INT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT,
   burger_name VARCHAR(255),
-  devoured BOOLEAN
+  devoured BOOLEAN,
+  PRIMARY KEY (id)
 );
 
---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+/* Seeds for SQL table. We haven't discussed this type of file yet */
+USE burgers_db;
+
+/* Insert 3 Rows into your new table */
+INSERT INTO burgers (burger_name)
+VALUES ("Cheese Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Veggie Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Salmon Burger");
+
