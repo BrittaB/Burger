d522204b5ec8dace326d1c3b9c4dfe2549cec6e7
Britta <polished.athena@gmail.com>
2019-05-24 21:25:40 -0500

    updated gitignore

--- server.js
+++ server.js
@@ -1,1 +1,5 @@
 const express = require("express");
+const PORT = process.env.PORT || 8080;
+const app = express();
+const exphbs = require("express-handlebars");
+const routes = require("./controllers/catsController.js");
