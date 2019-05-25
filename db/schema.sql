edd5214a3fcfbc640dcdceb8568054465b4e34bf
Britta <polished.athena@gmail.com>
2019-05-24 20:53:07 -0500

    updated table sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,9 @@
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 CREATE TABLE burgers (
-  id INT,
-  burger_name VARCHAR(""),
+  id PRIMARY KEY AUTO_INCREMENT,
+  burger_name VARCHAR(255),
   devoured BOOLEAN,
   PRIMARY KEY (id)
 );
